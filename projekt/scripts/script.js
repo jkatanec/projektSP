85ae259caa94cc44a5a69f32ef0a6164ae4e4e7e
jkatanec <j.katanec@gmail.com>
2016-11-27 17:15:04 +0100

    projekt

---
+++ projekt/scripts/script.js
@@ -0,0 +1,44 @@
+//menu-accordian:
+
+function accordian(x) {
+    if (x.className.indexOf("active") == -1) {
+        x.className += " active";
+        x.previousElementSibling.className += " open";
+    } else {
+        x.className = x.className.replace(" active", "");
+        x.previousElementSibling.className =
+        x.previousElementSibling.className.replace(" open", "");
+    }
+}
+function accordian1() {
+    var x = document.getElementById("acc1");
+    accordian(x);
+}
+function accordian2() {
+    var x = document.getElementById("acc2");
+    accordian(x);
+}
+
+
+//date and time on page:
+function startTime() {
+    
+    var now = new Date();
+    var h = now.getHours();
+    var m = now.getMinutes();
+    var s = now.getSeconds();
+    var d = now.getDate();
+    var mn = now.getMonth();
+    var y = now.getFullYear();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('time').innerHTML = d + "." + mn + "." + y + ", "+ h + ":" + m;
+    var t = setTimeout(startTime, 500);
+}
+function checkTime(i) {
+    if (i < 10) { i = "0" + i };  // add zero in front of numbers < 10
+    return i;
+}
+function goBack() {
+    window.history.back();
+}
